4cf8e9a3197b91a7000da8c54ea516c7c9bfc90f
Christopher Bejar <b.bejar.christopher@gmail.com>
2024-04-17 16:15:46 -0500

    testing for serial
    Serial communication for both software and hardware added

--- Central_Hub/Central_Hub.ino
+++ Central_Hub/Central_Hub.ino
@@ -10,9 +10,13 @@
 
 */
 
-
 // include the library code
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+// declare rx/tx pins
+#define rxPin 5
+#define txPin 6
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -22,16 +26,31 @@ const int buzzerPin = 7;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int buttonState = 0;  // variable for reading the pushbutton status
-bool startedGame = false; // variable used to determine if game started
+int buttonState = 0;  // for reading the pushbutton status
+bool startedGame = false; // to determine if game started
 
 unsigned long startTime = 0;
 unsigned long currTime = 0;
 
+SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
+
+// ######################################################### HELPER FUNCTIONS
+//
+//  playBuzz(frequency, duraction)
+//
+//  Plays a tone on the buzzer at specified frequency
+//  and for the specified duration of time in milliseconds
+//
 void playBuzz(int frequency, int duration) {
   tone(buzzerPin, frequency, duration);
 }
 
+//
+//  gameCountdown()
+//
+//  displays onto the LCD the countdown for the game to
+//  start. Plays a tone on the buzzer as it does so.
+//
 void gameCountdown() {
   int time = 3;
   while(true)
@@ -42,42 +61,79 @@ void gameCountdown() {
     lcd.print(" START GAME IN");
     lcd.setCursor(0,1);
     lcd.print("       " + String(time));
-    delay(1000); //should 1000, for one second
+    delay(1000); // one second
     time--;
     playBuzz(1500, 100);
-    if(time < 0){playBuzz(2500, 500); break;} //1500 freq
-    //print countdown and break out of loop
+    if(time < 0) {playBuzz(2500, 250); break;}
   }
 }
 
+int receivedSoftware() {
+  if (mySerial.available() > 0)
+  {
+    String receive = mySerial.readStringUntil('\n');
+    Serial.println("Received: " + receive);
+    return receive.toInt();
+  }
+  return -1;
+}
+
+int receivedHardware() {
+  if (Serial.available() > 0)
+  {
+    String receive = Serial.readStringUntil('\n');
+    Serial.println("Received: " + receive);
+    return receive.toInt();
+  }
+  return -1;
+}
+
+// ################################################################### SETUP
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+
+  // SOFTWARE SERIAL
+  // set up rx/tx pins as input/output
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  Serial.begin(9600);
+
   startTime = millis();
 }
 
+// #################################################################### LOOP
 void loop() {
+  receivedHardware();
+  return; // temporary return to test
+
   buttonState = digitalRead(buttonPin);
   
   if(startedGame) {
     gameCountdown();
     while(true) {
-      analogWrite(A0, LOW);
       lcd.clear();
       lcd.print("WE Gaming");
-      
+      delay(1000);
+      startedGame = false;
+      break;
     }
+  // Before game is started
   } else {
     lcd.setCursor(0,0);
-    lcd.print("Start Game?");
+    lcd.print("   Start Game?");
     lcd.setCursor(0,1);
-    lcd.print("Press Start");
+    lcd.print("   Press Start");
     
+    // Button pressed
     if(buttonState == HIGH)
     {
-      startedGame = !startedGame;
+      startedGame = true;
     }
   }
 }
