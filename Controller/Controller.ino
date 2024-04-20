cb721bc7bdd0c3953216a9252f2b7bf2d2487e75
Christopher Bejar <b.bejar.christopher@gmail.com>
2024-04-19 19:30:56 -0500

    compile scores function
    added function to change score values and also changed serial to get a char instead of an int because we only really need to know which input was sent. We can compare in the central hub

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,9 +1,47 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String readString;
+  String Q;
 
+  while(Serial1.available()) {
+    delay(1);
+    if (Serial1.available() > 0) {
+      char c = Serial1.read();
+      Serial.println(c);
+      if (isControl(c)) {
+        break;
+      }
+    }
+  }
+
+  while (Serial.available()) {
+    delay(1);
+    if (Serial.available() > 0) {
+      char c = Serial.read();
+      if (isControl(c)) {
+        break;
+      }
+
+      readString += c;
+    }
+  }
+    
+  Q = readString;
+
+  if (Q = "on") {
+    Serial1.print("1");
+    Serial.println("Sent: On");
+  }
+  if (Q = "off") {
+    Serial1.print("2");
+    Serial.println("Sent: Off");
+  }
+
+  
 }
